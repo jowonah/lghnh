8946a67c89d1ce609c5219bf9a0eacffff6b92e5
jowonah <86401976+jowonah@users.noreply.github.com>
2021-07-19 14:59:26 +0900

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+$(".slide-bg-img-slide").slick({
+    arrows:false,
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay:true,
+    autoplaySpeed:5000
+});
