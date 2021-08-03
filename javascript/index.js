d5e096c219a6c36664b159baac29b0684a7ae918
jowonah <86401976+jowonah@users.noreply.github.com>
2021-08-03 12:50:44 +0900

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,28 @@
+$(function(){
+    $('header').load('inc.html header .head',select);
+
+    function select(){
+        $('header div a').on('click',function(){
+            $(this).css('color','#c40352');
+        });
+
+                
+        const mn1Ul = document.querySelectorAll('.mn-1 li');
+        const hidMn = document.querySelectorAll('.hid-mn');
+
+        for(let k=0; k<mn1Ul.length; k++){
+            mn1Ul[k].addEventListener('mouseover',function(){
+                hidMn[k].style = 'display:flex;';
+                hidMn[k].addEventListener('mouseleave',function(){
+                    hidMn[k].style = 'display:none;';
+                })
+            })
+            
+        }
+
+    }
+});
+
 $(".slide-bg-img-slide").slick({
     arrows:false,
     dots: true,
@@ -5,5 +30,57 @@ $(".slide-bg-img-slide").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay:true,
-    autoplaySpeed:5000
+    autoplaySpeed:3300,
+    speed: 800,
+    fade: true,
+    cssEase: 'linear',
 });
+$('.slide-bg-img-slide').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    console.log(33 * (nextSlide+1) + '%');
+  });
+
+
+
+const elDiv = document.querySelectorAll('.main > div');
+    let elHei;
+    let winHei = window.innerHeight;
+        
+    window.addEventListener('scroll',function(){
+        for(let i=0; i<elDiv.length; i++){
+            elHei = elDiv[i].offsetTop;
+
+            if(elHei-winHei <= window.scrollY){
+                elDiv[i].classList.add('active');
+            }
+        }
+        
+    })
+
+
+
+
+const slides = document.querySelector('.slides');
+const slideItem = slides.querySelectorAll('li');
+const prevBtn = document.querySelectorAll('.btn-con2 a');
+
+let itemWid = slideItem[0].offsetLeft * 2 + slideItem[0].offsetWidth;
+
+
+let sIdx = 0;
+slideItem[2].classList.add('active');
+for(let i=0; i<prevBtn.length; i++){
+    prevBtn[i].addEventListener('click',function(){
+      if(i==1){
+          if(sIdx<7)sIdx++;
+      }else{
+          if(sIdx>-2)sIdx--;
+      }
+
+      for(let j=0; j<slideItem.length; j++){
+        slideItem[j].classList.remove('active');    
+      }
+      slideItem[sIdx+2].classList.add('active');
+
+      slides.style=`transform:translateX(${-itemWid*sIdx}px)`;
+  });
+}
---
+++ javascript/json.js
@@ -0,0 +1,31 @@
+function index(){ 
+
+    fetch("./javascript/data.json")
+    .then(res => res.json() )
+    .then(data => callback(data) );
+
+        const popup = document.querySelector('.popup');
+        const elLi = document.querySelectorAll('.ideo > li');
+
+        function callback(data){
+            let tagList='';
+
+            for(let i=0; i<elLi.length; i++){
+                elLi[i].addEventListener('click',function(){
+                    popup.style = 'display:flex';
+                    dataChange(i);
+                });
+            }
+            function dataChange(i){
+                title = data.work[i].title;
+                photo = data.work[i].photo;
+
+                tagList =`<div><img src="${photo}"></div>`;
+                popup.innerHTML = tagList;
+            }
+        }
+         popup.addEventListener('click',function(){
+                popup.style = 'display:none';
+            })
+}
+window.onload = index;
